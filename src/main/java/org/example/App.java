ff47f05e040021f2fe1c50ffa6d297b3975e6938
Emmanuel Samandoulougou <samabdoulougoue@gmail.com>
2023-01-03 21:18:30 +0000

    Creation of new maven quickstart archetype project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(
+                "Spring Framework, at its core, is a dependency injection container that manages the classes you wrote" +
+                        " and their dependencies for you!" );
+
+    }
+}
